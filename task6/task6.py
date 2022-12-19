f47e5d5f972c50dffacf34ceee27bcaaab87df11
MVVladimir <mvv1_1@mail.ru>
2022-12-20 01:04:46 +0300

    task6

---
+++ task6/task6.py
@@ -0,0 +1,36 @@
+import numpy as np
+import json
+
+def task(s: str) -> np.array:
+    df = np.array(json.loads(s))
+
+    r = []
+    for i in range(df.shape[1]):
+        r_i = []
+        for j in range(df.shape[0]):
+            r_ij = []
+            for k in range(df.shape[0]):
+                if df[j][i] == df[k][i]:
+                    r_ij.append(0.5)
+                elif df[j][i] > df[k][i]:
+                    r_ij.append(0)
+                else:
+                    r_ij.append(1)
+            r_i.append(r_ij)
+        r.append(r_i)
+
+    r = np.array(r)
+
+    x = r.mean(0)
+
+    k = np.array([1/3, 1/3, 1/3])
+    k_old = k.copy()
+
+    E = 0.001
+    while np.linalg.norm(k - k_old) < E:
+        k_old = k.copy()
+        y = x @ k
+        lmd = np.array([1.0, 1.0, 1.0]) @ (x @ k)
+        k = (1 / lmd) * y
+
+    return k.tolist()
